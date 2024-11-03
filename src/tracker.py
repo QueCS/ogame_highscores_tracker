74af03fb2dfe04c67fcd5c92ee4490458e66ab60
QueCS <73703089+QueCS@users.noreply.github.com>
2024-11-03 20:12:25 +0100

    working implementation

--- src/tracker.py
+++ src/tracker.py
@@ -12,54 +12,45 @@ os.chdir(f"{os.path.dirname(__file__)}")
 with open("../config.toml", "rb") as config_file:
     config = tomllib.load(config_file)
 
-log_dir = config.get("SCRIPT", {}).get("log_dir")
-log_lvl_str = config.get("SCRIPT", {}).get("log_lvl")
-module_name, attribute_name = log_lvl_str.rsplit(".", 1)
-log_lvl = getattr(logging, attribute_name)
-
-host = config.get("INFLUXDB", {}).get("host")
-org = config.get("INFLUXDB", {}).get("org")
-database = config.get("INFLUXDB", {}).get("database")
-token = config.get("INFLUXDB", {}).get("token")
-client = InfluxDBClient3(token=token, org=org, host=host, database=database)
-
 
 def main():
-    servers = config.get("OGAME", {}).get("servers")
-    cats = config.get("OGAME", {}).get("categories")
-    typs = config.get("OGAME", {}).get("types")
+    # Parse config file
+    log_dir, log_lvl = get_logging_config(config)
+    client = get_influxdb_client(config)
+    servers, cats, typs = get_ogame_config(config)
+
+    # Indefinitely iterate over all combinations of server, category and type
     iterations = len(servers) * len(cats) * len(typs)
-    for server, cat, typ in itertools.product(servers, cats, typs):
-        process_task(server, cat, typ)
-        time.sleep(900 / iterations)
+    while True:
+        for server, cat, typ in itertools.product(servers, cats, typs):
+            # Logging setup
+            logger = logging.getLogger(f"{server}_{cat}_{typ}_logger")
+            logger.setLevel(log_lvl)
+            log_filename = f"{log_dir}/{server}_{cat}_{typ}_tracker.log"
+            if not os.path.exists(os.path.dirname(log_filename)):
+                os.makedirs(os.path.dirname(log_filename))
+            file_handler = logging.FileHandler(log_filename)
+            formatter = logging.Formatter(
+                "%(asctime)s %(levelname)s - %(funcName)s(): %(message)s"
+            )
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+            # Fetch data from API and update database
+            data = fetch_api(server, cat, typ, logger)
+            if data is None:
+                continue
+            update_db(data, server, cat, typ, client, logger)
+
+            # Detach logging handler to prevent duplicate logs
+            logger.removeHandler(file_handler)
+
+            # Sleep so total loop time is ~15 minutes
+            time.sleep(900 / iterations)
+            continue
 
 
-def process_task(server, cat, typ):
-    """
-    Check if the API has been updated and updates the database accordingly.
-    """
-    logger = logging.getLogger(f"{server}_{cat}_{typ}_tracker")
-    logger.setLevel(log_lvl)
-    log_filename = f"{log_dir}/{server}_{cat}_{typ}_tracker.log"
-    if not os.path.exists(os.path.dirname(log_filename)):
-        os.makedirs(os.path.dirname(log_filename))
-    file_handler = logging.FileHandler(log_filename)
-    formatter = logging.Formatter(
-        "%(asctime)s %(levelname)s - %(funcName)s(): %(message)s"
-    )
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    old_timestamp = 0
-    data = fetch_api(server, cat, typ, logger)
-    new_timestamp, api_updated = check_if_api_updated(data, old_timestamp, logger)
-    if api_updated:
-        old_timestamp = new_timestamp
-        update_db(data, new_timestamp, server, cat, typ, client, logger)
-        return True
-    return False
-
-
-def fetch_api(server: str, cat: str, typ: str, logger: logging.Logger) -> dict:
+def fetch_api(server: str, cat: str, typ: str, logger: logging.Logger) -> dict | None:
     """
     Fetches and returns OGame "highscores" API data.
 
@@ -70,70 +61,41 @@ def fetch_api(server: str, cat: str, typ: str, logger: logging.Logger) -> dict:
 
     Returns:
         data (dict): A dictionnary of the "highscores" API JSON response.
+        None if there is an error while making the API request or decoding the JSON-formatted data.
 
     Raises:
         requests.exceptions.RequestException: If there is an error while making the API request.
         json.JSONDecodeError: If the JSON-formatted data cannot be decoded into a Python object.
     """
     url = f"https://s{server}.ogame.gameforge.com/api/highscore.xml?toJson=1&category={cat}&type={typ}"
-    logger.info(f"Fetching data from {url}.")
+    logger.info(f"Fetching {url}")
     try:
         response = requests.get(url)
     except requests.exceptions.RequestException as e:
-        logger.warning(f"Failed with status code: {e}.")
+        logger.warning(f"Failed fetching the API: {e}")
         return None
     if response.status_code != 200:
-        logger.warning(f"Failed with status code: {response.status_code}.")
+        logger.warning(f"Failed fetching the API: {response.status_code}")
         return None
-    logger.info("Successfully fetched data.")
     try:
         data = json.loads(response.text)
     except json.JSONDecodeError as e:
-        logger.warning(f"Failed to decode JSON response: {e}.")
+        logger.warning(f"Failed decoding JSON response: {e}")
         return None
-    logger.info("Successfully parsed data.")
+    logger.info("Successfully fetched API and parsed data")
     return data
-
-
-def check_if_api_updated(
-    data: dict, old_timestamp: int, logger: logging.Logger
-) -> (int, bool):
-    """
-    Checks if the API data has been updated since the last fetch.
-
-    Args:
-        data (dict): A dictionnary of the "highscores" API JSON response.
-        old_timestamp (int): The timestamp of the last fetched data.
-
-    Returns:
-        bool: True if the API data has been updated, False otherwise or if a KeError is raised.
-
-    Raises:
-        KeyError: If the "timestamp" key is not found in the "@attributes" section of the data.
-    """
-    try:
-        new_timestamp = int(data["@attributes"]["timestamp"])
-    except KeyError as e:
-        logger.warning(f"Failed to find timestamp in API response: {e}.")
-        return False
-    if new_timestamp > old_timestamp:
-        logger.info("API updated.")
-        return new_timestamp, True
-    else:
-        logger.info("API not updated yet.")
-        return old_timestamp, False
 
 
 def update_db(
     data: dict,
-    timestamp: int,
     server: str,
     cat: str,
     typ: str,
     db_client: InfluxDBClient3,
     logger: logging.Logger,
 ) -> None:
-    logger.info("Parsing data and updating database...")
+    logger.info("Parsing data and updating database")
+    timestamp = int(data["@attributes"]["timestamp"])
     points = []
     if typ == 0:
         highscore_type = "general"
@@ -160,7 +122,7 @@ def update_db(
     elif typ == 11:
         highscore_type = "lf_discovery"
     else:
-        logger.warning(f"Invalid highscore type: {typ}.")
+        logger.warning(f"Invalid highscore type: {typ}")
         highscore_type = "unkown"
     if cat == 1:
         highscore_category = "player"
@@ -215,10 +177,39 @@ def update_db(
             )
             points.append(point)
     else:
-        logger.warning(f"Invalid highscore category: {cat}.")
+        logger.warning(f"Invalid highscore category: {cat}")
         highscore_category = "unknown"
-    db_client.write(points)
-    logger.info("Done !")
+    try:
+        db_client.write(points)
+    except Exception as e:
+        logger.error(f"Failed writing to database: {e}")
+        return None
+    logger.info("Done")
+    return None
+
+
+def get_logging_config(config):
+    log_dir = config.get("SCRIPT", {}).get("log_dir")
+    log_lvl_str = config.get("SCRIPT", {}).get("log_lvl")
+    module_name, attribute_name = log_lvl_str.rsplit(".", 1)
+    log_lvl = getattr(logging, attribute_name)
+    return log_dir, log_lvl
+
+
+def get_influxdb_client(config):
+    host = config.get("INFLUXDB", {}).get("host")
+    org = config.get("INFLUXDB", {}).get("org")
+    database = config.get("INFLUXDB", {}).get("database")
+    token = config.get("INFLUXDB", {}).get("token")
+    client = InfluxDBClient3(token=token, org=org, host=host, database=database)
+    return client
+
+
+def get_ogame_config(config):
+    servers = config.get("OGAME", {}).get("servers")
+    cats = config.get("OGAME", {}).get("categories")
+    typs = config.get("OGAME", {}).get("types")
+    return servers, cats, typs
 
 
 if __name__ == "__main__":
